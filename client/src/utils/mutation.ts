479949fa3f61cbc6a9813b8480c3f72b091e9670
rs0579 <raheemsenegal@outlook.com>
2025-03-15 06:47:17 +0100

    made changes to the mutations and the App.tsx in the server side code

--- client/src/utils/mutation.ts
+++ client/src/utils/mutation.ts
@@ -4,28 +4,44 @@ export const LOGIN_USER = gql`
 mutation login($email:String!, $password:String!){
 login(email: $email, password: $password){
     token
+    email
+    password
 }
 }
 `;
 
 export const ADD_USER = gql`
 mutation addUser($username: String!, $email:String!, $password: String!){
-    addUser(username: $username, email: $email, password: $password)
-}
-`;
-
-export const SAVE_BOOK = gql`
-mutation saveBook($userId:String!, $book:String!){
-    saveBook(userID: $userID, book:$book){
-
+    addUser(username: $username, email: $email, password: $password) {
+        username
+        email
+        password
     }
 }
 `;
 
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInput!){
+    saveBook(userID: $userID, BookInput:$input){
+        userID
+        input
+    }
+}
+`;
+
+// export const SAVE_BOOK = gql`
+// mutation saveBook($userId:String!, $book:String!){
+//     saveBook(userID: $userID, book:$book){
+//         userID
+//         book
+//     }
+// }
+// `;
+
 export const REMOVE_BOOK = gql`
-mutation removeBook($book:String){
-    removeBook(book:$book){
-        
+mutation removeBook($bookId:ID){
+    removeBook(bookId:$bookId){
+        bookId
     }
 }
 `;
