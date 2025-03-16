83470b23c11b545ab3d7a961207b43b3b20f0310
rs0579 <raheemsenegal@outlook.com>
2025-03-16 22:56:25 +0100

    modified the loginForm function

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -3,9 +3,12 @@ import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
+// import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutation';
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 const LoginForm = ({}: { handleModalClose: () => void }) => {
@@ -18,6 +21,7 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  const [login] = useMutation(LOGIN_USER)
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -29,13 +33,18 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await login({
+        variables:{
+          email: userFormData.email,
+          password: userFormData.password
+        }
+      });
 
-      if (!response.ok) {
+      if (!response.data) {
         throw new Error('something went wrong!');
       }
 
-      const { token } = await response.json();
+      const { token } = await response.data.login;
       Auth.login(token);
     } catch (err) {
       console.error(err);
